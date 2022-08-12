4a68305a0b49556ef3a3168d62dfb21ec3e19c99
raj <rajkumarhalburge17@gmail.com>
2022-08-13 01:20:07 +0530

    im adding for fb

--- Goodreads/src/test/java/automategoodreads/Testgoodreads.java
+++ Goodreads/src/test/java/automategoodreads/Testgoodreads.java
@@ -7,7 +7,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.SendKeysAction;
 import org.testng.annotations.AfterTest;
 
 public class Testgoodreads {
@@ -23,7 +26,7 @@ public class Testgoodreads {
 	  driver.manage().window().maximize();
   }
  
-  @Test
+  @Test(enabled=false)
   public void ByEmail() {
 	  driver.findElement(By.linkText("Sign up with email")).click();
 	  //driver.findElement(By.xpath("//*[@id='signInUsingContent']/a[4]")).click();
@@ -31,10 +34,24 @@ public class Testgoodreads {
 	  driver.findElement(By.name("email")).sendKeys("mehtamadarchod@gmail.com");
 	  driver.findElement(By.id("ap_password")).sendKeys("gandu@123");
 	  driver.findElement(By.xpath("//input[@id='ap_password_check']")).sendKeys("gandu@123");
-	  driver.findElement(By.xpath("//span[@id='a-autoid-0']")).click();
-	 
+	  driver.findElement(By.xpath("//span[@id='a-autoid-0']")).click();	 
   }
- 
+ @Test
+ public void ByFb()
+ {
+	WebElement d= driver.findElement(By.xpath("//*[@id='signInUsingContent']/a[1]"));
+	 Actions click= new Actions(driver);
+	 click.moveToElement(d).click().build().perform();
+	// driver.navigate().back();
+	 
+	 
+	 
+	//WebElement fb= driver.findElement(By.id("email")).sendKeys("8951560426");
+	 
+	 
+	 
+	 
+ }
 
  
   
